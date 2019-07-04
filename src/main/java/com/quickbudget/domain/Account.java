40e7f3a3dc1f623bb69efcfdc38a2e7583054fb4
dazz4 <kaminski.darek@icloud.com>
2019-07-04 08:49:50 +0100

    created entities and joined columns

--- src/main/java/com/quickbudget/domain/Account.java
+++ src/main/java/com/quickbudget/domain/Account.java
@@ -3,17 +3,25 @@ package com.quickbudget.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Entity(name = "accounts")
+@Setter
+@Entity
+@Table(name = "ACCOUNTS")
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
+    @Column(name = "id", unique = true)
     private Long id;
 
     @Column(name = "name")
@@ -21,4 +29,16 @@ public class Account {
 
     @Column(name = "balance")
     private BigDecimal balance;
+
+    @ManyToOne
+    @JoinColumn(name = "budget_id")
+    private Budget budget;
+
+    @OneToMany(
+            targetEntity = Transaction.class,
+            mappedBy = "account",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    private List<Transaction> transactions = new ArrayList<>();
 }
